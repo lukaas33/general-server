e5cc1342150bc89445081217c38f096aa872aa47
lukaas33 <lucasvanosenbruggen@gmail.com>
2017-12-24 17:01:26 +0100

    Moved code for chat to module

--- index.js
+++ index.js
@@ -21,19 +21,13 @@ const chat = require('./modules/chat-app')
 // Variables
 const app = express()
 const event = {
-  general: new events.EventEmitter(),
-  chat: new events.EventEmitter()
+  general: new events.EventEmitter()
 }
-var activeUsers = {}
 
 // Events
 event.general.on('day', () => {
   mail.send(mail.templates.status)
 })
-
-var db = require('./modules/database')
-db.query('DELETE FROM messages')
-db.query('DELETE FROM users')
 
 
 // Setup
@@ -59,66 +53,7 @@ app.get('/', function (request, response) {
   response.send('Web client response')
 })
 
-
-app.post('/chat/createuser', function (request, response) {
-  console.log(request.body)
-  try {
-    chat.user('add', request.body) // Add to database
-    response.end('success')
-    activeUsers[request.body.ID] = false // Track
-    let check = setInterval((id) => {
-      console.log('# Chat', 'Exists', id)
-
-      if (activeUsers[id]) { // Has checked in
-        activeUsers[id] = false // For next check
-      } else { // Hasn't checked in in time
-        chat.user('remove', id) // Delete records
-        console.log('# Chat', 'Delete', id)
-        delete activeUsers[id]
-        clearInterval(check)
-      }
-    }, 15000, request.body.ID)
-
-  } catch (error) {
-    console.log('# Chat', 'User not created', error)
-    response.end('error')
-  }
-})
-
-app.post('/chat/sendmessage', function (request, response) {
-  console.log(request.body)
-  try {
-    chat.message(request.body)
-    response.end('success')
-  } catch (error) {
-    response.end('error')
-  }
-})
-
-app.get('/chat/data/:id', function (request, response) {
-  if (request.params.id.indexOf('-') !== -1) {
-    try {
-      chat.get('messages', request.params.id.split('-'), (data) => {
-        response.end(JSON.stringify(data))
-      })
-    } catch (error) {
-      console.log('# Chat', 'Data getting', error)
-      response.end('error')
-    }
-
-  } else {
-    console.log('# Chat', 'Check in', request.params.id)
-    activeUsers[request.params.id] = true // Checks in
-    try {
-      chat.get('users', request.params.id, (data) => { // Request for chats
-        response.end(JSON.stringify(data))
-      })
-    } catch (error) {
-      console.log('# Chat', 'Data getting', error)
-      response.end('error')
-    }
-  }
-})
+chat.setup(app) // The app
 
 
 app.listen(app.get('port'), () => {
--- modules/chat-app.js
+++ modules/chat-app.js
@@ -1,4 +1,5 @@
 const database = require('./database')
+var activeUsers = {}
 
 const user = function (action, values) {
   if (action === 'add') { // Add new user
@@ -37,4 +38,69 @@ const get = function (needed, ids, callback) {
 
 }
 
-module.exports = {user, message, get}
+const setup = function (app) {
+  database.query('DELETE FROM messages')
+  database.query('DELETE FROM users')
+
+  app.post('/chat/createuser', function (request, response) {
+    console.log(request.body)
+    try {
+      user('add', request.body) // Add to database
+      response.end('success')
+      activeUsers[request.body.ID] = false // Track
+      let check = setInterval((id) => {
+        console.log('# Chat', 'Exists', id)
+
+        if (activeUsers[id]) { // Has checked in
+          activeUsers[id] = false // For next check
+        } else { // Hasn't checked in in time
+          user('remove', id) // Delete records
+          console.log('# Chat', 'Delete', id)
+          delete activeUsers[id]
+          clearInterval(check)
+        }
+      }, 15000, request.body.ID)
+
+    } catch (error) {
+      console.log('# Chat', 'User not created', error)
+      response.end('error')
+    }
+  })
+
+  app.post('/chat/sendmessage', function (request, response) {
+    console.log(request.body)
+    try {
+      message(request.body)
+      response.end('success')
+    } catch (error) {
+      response.end('error')
+    }
+  })
+
+  app.get('/chat/data/:id', function (request, response) {
+    if (request.params.id.indexOf('-') !== -1) {
+      try {
+        get('messages', request.params.id.split('-'), (data) => {
+          response.end(JSON.stringify(data))
+        })
+      } catch (error) {
+        console.log('# Chat', 'Data getting', error)
+        response.end('error')
+      }
+
+    } else {
+      console.log('# Chat', 'Check in', request.params.id)
+      activeUsers[request.params.id] = true // Checks in
+      try {
+        get('users', request.params.id, (data) => { // Request for chats
+          response.end(JSON.stringify(data))
+        })
+      } catch (error) {
+        console.log('# Chat', 'Data getting', error)
+        response.end('error')
+      }
+    }
+  })
+}
+
+module.exports = {user, message, get, setup}
